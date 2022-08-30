e7938c80411dcdddf530456081f4cd9e071b0a05
Singharaj Usai <djjd22938@gmail.com>
2022-08-30 17:26:48 -0400

    server js finished

--- index.js
+++ index.js
@@ -26,5 +26,44 @@ app.get('/', (req, res) =>
     res.render(path.join(__dirname, "public/notes"))
 );
 
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+      };
+  
+          // Convert the data to a string so we can save it
+    const noteString = JSON.stringify(newNote);
+
+      // Write the string to a file
+      fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Review for ${newNote.title} has been written to JSON file`
+          )
+    );
+      const response = {
+        status: 'success',
+        response: newNote
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting notes');
+    }
+  });
+
 // Server Start
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
