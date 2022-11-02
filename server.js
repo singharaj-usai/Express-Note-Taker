cd843a80220f233a8bf3729ffde33c942ad90ef8
singharaj usai <djjd22938@gmail.com>
2022-11-02 14:19:23 -0400

    file structure and routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -14,6 +16,10 @@ app.set('view engine', 'handlebars');
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 // Server Start
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
