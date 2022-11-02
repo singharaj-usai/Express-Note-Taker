128d3a57ddb0c6a4890fc2ba68eb30bbdb022d97
singharaj usai <djjd22938@gmail.com>
2022-11-02 15:39:14 -0400

    {notes} in notesRoutes.js

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { createNewNote, filterByQuery, validateNote, findById } = require("../../lib/notes.js");
-const notes = require("../../db/db.json");
+const {notes} = require("../../db/db.json");
 
 router.get("/notes", (req, res) => {
     let results = notes;
